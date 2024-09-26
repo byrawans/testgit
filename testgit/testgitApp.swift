80337ae3ae885ad27453ab63689e86ba0f99d278
byrawans <raw7aan@gmail.com>
2024-09-26 17:11:46 +0300

    Initial Commit

---
+++ testgit/testgitApp.swift
@@ -0,0 +1,17 @@
+//
+//  testgitApp.swift
+//  testgit
+//
+//  Created by Rawan Alshareef on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct testgitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
